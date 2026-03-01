23633296631653bf6080c994865359318ddd04e6
MeowVal <meowval@catkatnya.com>
2026-03-01 15:07:50 +0100

    fixed some warnings
    port of Cleanup warnings in AI wire actions#37780 by Tayrtahn

--- Content.Server/Silicons/StationAi/AiInteractWireAction.cs
+++ Content.Server/Silicons/StationAi/AiInteractWireAction.cs
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
@@ -29,13 +30,13 @@ public sealed partial class AiInteractWireAction : ComponentWireAction<StationAi
     public override bool Cut(EntityUid user, Wire wire, StationAiWhitelistComponent component)
     {
         return EntityManager.System<SharedStationAiSystem>()
-            .SetWhitelistEnabled((component.Owner, component), false, announce: true);
+            .SetWhitelistEnabled((wire.Owner, component), false, announce: true);
     }
 
     public override bool Mend(EntityUid user, Wire wire, StationAiWhitelistComponent component)
     {
         return EntityManager.System<SharedStationAiSystem>()
-            .SetWhitelistEnabled((component.Owner, component), true);
+            .SetWhitelistEnabled((wire.Owner, component), true);
     }
 
     public override void Pulse(EntityUid user, Wire wire, StationAiWhitelistComponent component)
--- Content.Server/Silicons/StationAi/AiVisionWireAction.cs
+++ Content.Server/Silicons/StationAi/AiVisionWireAction.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
@@ -31,13 +32,13 @@ public sealed partial class AiVisionWireAction : ComponentWireAction<StationAiVi
     public override bool Cut(EntityUid user, Wire wire, StationAiVisionComponent component)
     {
         return EntityManager.System<SharedStationAiSystem>()
-            .SetVisionEnabled((component.Owner, component), false, announce: true);
+            .SetVisionEnabled((wire.Owner, component), false, announce: true);
     }
 
     public override bool Mend(EntityUid user, Wire wire, StationAiVisionComponent component)
     {
         return EntityManager.System<SharedStationAiSystem>()
-            .SetVisionEnabled((component.Owner, component), true);
+            .SetVisionEnabled((wire.Owner, component), true);
     }
 
     public override void Pulse(EntityUid user, Wire wire, StationAiVisionComponent component)
